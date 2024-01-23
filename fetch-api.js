e74801cc7cfcbd3e29700401cd6371434ccc1593
Douglinhas <daguiaralcantara@gmail.com>
2024-01-23 17:30:22 -0300

    feat: create fetch-api functions

---
+++ fetch-api.js
@@ -0,0 +1,55 @@
+const urlBase = "https://api.box3.work/api/Contato";
+
+export const getContacts = async () => {
+  try {
+    const response = await fetch(url);
+    const lista = await response.json();
+    return lista;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const createContact = async (contact) => {
+  try {
+    const response = await fetch(urlBase, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(contact),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
+export const deleteContact = async (id) => {
+  try {
+    const response = await fetch(`${urlBase}/${id}`, {
+      method: "DELETE",
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
+export const updateContact = async (id, contact) => {
+  try {
+    const response = await fetch(`${urlBase}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(contact),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
